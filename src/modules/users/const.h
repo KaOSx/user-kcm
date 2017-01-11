038a13a18b43661db6e489328080e168a52faeb4
demmm <anke62@gmail.com>
2017-01-11 13:19:52 -0500

    set passwordless printer access

--- src/modules/users/const.h
+++ src/modules/users/const.h
@@ -29,7 +29,7 @@
 #define PASSWD "/etc/passwd"
 #define GROUPCONF "/etc/group"
 #define FACES_IMAGE_FOLDER "/usr/share/pixmaps/faces"
-#define DEFAULT_USER_GROUPS "audio,lp,network,optical,scanner,storage,users,video"
+#define DEFAULT_USER_GROUPS "audio,lp,network,optical,scanner,storage,users,video,lpadmin"
 #define ADMIN_GROUP "wheel"
 #define SUDOERSFILE "/etc/sudoers"
 
